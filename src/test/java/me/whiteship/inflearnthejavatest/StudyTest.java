aada75ba199bbf4b9bf4f8311da49e4a01375ff6
JangYeonji <yunji0220@naver.com>
2022-11-14 21:39:39 +0900

    JUnit5 Assertion

--- src/test/java/me/whiteship/inflearnthejavatest/StudyTest.java
+++ src/test/java/me/whiteship/inflearnthejavatest/StudyTest.java
@@ -2,6 +2,10 @@ package me.whiteship.inflearnthejavatest;
 
 import org.junit.jupiter.api.*;
 
+import java.lang.reflect.Executable;
+import java.time.Duration;
+import java.util.function.Supplier;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -9,9 +13,35 @@ class StudyTest {
     @Test
     @DisplayName("스터디 만들기")
     void create_new_study() {
-        Study study = new Study();
-        assertNotNull(study);
-        System.out.println("create");
+        assertTimeoutPreemptively(Duration.ofMillis(100), () -> {
+            new Study(10);
+            Thread.sleep(300);
+        });
+        //TODO ThreadLocal
+        //ThreadLocal을 사용하는 경우에는 테스트가 제대로 적용되지 않을 수 있음
+        //assertTimeoutPreemptively는 Thread와 관련 없을 때 쓰기
+
+
+//        assertTimeout(Duration.ofMillis(100), () -> {
+//            new Study(10);
+//            Thread.sleep(300);
+//        });
+
+
+//        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Study(-10));
+//        String message = exception.getMessage();
+//        assertEquals("limit은 0보다 커야 한다.", exception.getMessage());
+
+
+//        Study study = new Study(-10);
+//        assertNotNull(study);
+//        //람다식은 실패할 때만 메시지를 출력함. 그냥 메시지를 쓰는 것보다 람다를 쓰는게 성능이 더 좋아짐
+//        assertAll(
+//                () -> assertNotNull(study),
+//                () -> assertEquals(StudyStatus.DRAFT, study.getStatus(),
+//                        () -> "스터디를 처음 만들면 " + StudyStatus.DRAFT + " 상태다."),
+//                () -> assertTrue(study.getLimit() > 0, "스터디 최대 참석 가능 인원은 0보다 커야 한다.")
+//        );
     }
 
     @Test
