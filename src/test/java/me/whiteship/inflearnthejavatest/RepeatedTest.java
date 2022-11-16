8d82d929812b3f6b9a2ae262fc5aa3a00b7a18db
JangYeonji <yunji0220@naver.com>
2022-11-16 21:17:35 +0900

    JUnit5 테스트 반복하기

---
+++ src/test/java/me/whiteship/inflearnthejavatest/RepeatedTest.java
@@ -0,0 +1,59 @@
+package me.whiteship.inflearnthejavatest;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+
+public class RepeatedTest {
+
+    @FastTest
+    @DisplayName("스터디 만들기 fast")
+    void create_new_study() {
+        Study actual = new Study(100);
+        assertThat(actual.getLimit()).isGreaterThan(0);
+    }
+
+    @SlowTest
+    @DisplayName("스터디 만들기 slow")
+    @Tag("slow")
+    void create_new_study_again() {
+        System.out.println("create1");
+    }
+
+    @org.junit.jupiter.api.RepeatedTest(value = 10, name = "{displayName}, {currentRepetition}/{totalRepetitions}")
+    void repeatTest(RepetitionInfo repetitionInfo) {
+        System.out.println("test" + repetitionInfo.getCurrentRepetition() + "/" + repetitionInfo.getTotalRepetitions());
+    }
+
+    @DisplayName("스터디 만들기")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @ValueSource(strings = {"날씨가", "많이", "추워지고", "있네요"})
+    void parameterizedTest(String message) {
+        System.out.println(message);
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("before all");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("after all");
+    }
+
+    @BeforeEach
+    void beforeEachAll() {
+        System.out.println("before each");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("after each");
+    }
+
+}
